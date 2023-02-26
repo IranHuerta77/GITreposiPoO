66897a9dd88fc41d2c243d5441da06f433f06f45
IranHuerta77 <126007840+IranHuerta77@users.noreply.github.com>
2023-02-25 20:12:09 -0600

    Add files via upload
    TAREA 2

---
+++ TAREA2/Cadena.java
@@ -0,0 +1,11 @@
+public class Cadena{
+    public static void main(String[] args){
+        String a = "Carlos";
+        String b = "Estudiante";
+        String c = "México";
+        System.out.println("Nombre: " + a);
+        System.out.println("Profesión: " + b);
+        System.out.println("País: " + c);
+
+    }
+}
